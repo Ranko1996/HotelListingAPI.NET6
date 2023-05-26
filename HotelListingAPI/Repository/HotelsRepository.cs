5b22749c23d6ae6b4c14a4fcf5282e2ae41e7161
Ranko1996 <koturic96@gmail.com>
2023-05-26 09:30:23 +0200

    Added hotel endpoints

---
+++ HotelListingAPI/Repository/HotelsRepository.cs
@@ -0,0 +1,16 @@
+﻿using HotelListingAPI.Contracts;
+using HotelListingAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListingAPI.Repository
+{
+    public class HotelsRepository : GenericRepository<Hotel>, IHotelsRepository
+    {
+        public HotelListingDbConext _context { get; }
+        public HotelsRepository(HotelListingDbConext context) : base(context)
+        {
+            this._context = context;
+        }
+
+    }
+}
